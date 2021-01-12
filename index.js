d077d55c4fa5c94864df6151fc5be07a0317eeeb
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-11 20:43:10 -0800

    java jivin and vibin

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main#main').remove(); 
+
+var newHeader = document.createElement('h1');
+
+newHeader.id = "victory"; 
+
+newHeader.innerHTML = "Smol is the champion!"
+
